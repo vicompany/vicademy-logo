d5184254f738991037ecef44e1c272cae83ca70d
Tim Severien <timseverien@gmail.com>
2017-04-11 14:23:59 +0200

    Add workflow for images and CSS

--- gulpfile.js
+++ gulpfile.js
@@ -18,6 +18,14 @@ const paths = {
     source: path.join(DIR_SOURCE, 'scripts'),
     destination: path.join(DIR_DESTINATION, 'assets', 'scripts'),
   },
+  styles: {
+    source: path.join(DIR_SOURCE, 'styles'),
+    destination: path.join(DIR_DESTINATION, 'assets', 'styles'),
+  },
+  images: {
+    source: path.join(DIR_SOURCE, 'images'),
+    destination: path.join(DIR_DESTINATION, 'assets', 'images'),
+  },
   templates: {
     source: path.join(DIR_SOURCE, 'templates'),
     destination: DIR_DESTINATION,
@@ -40,13 +48,19 @@ const OPTIONS_ROLLUP_WRITE = {
 
 gulp.task('clean', () => del(paths.clean));
 
+gulp.task('images', () => gulp.src(`${paths.images.source}/**.{gif,jpg,png,svg}`)
+  .pipe(gulp.dest(paths.images.destination)));
+
 gulp.task('scripts', () => rollup.rollup(OPTIONS_ROLLUP)
   .then(bundle => bundle.write(OPTIONS_ROLLUP_WRITE)));
+
+gulp.task('styles', () => gulp.src(`${paths.styles.source}/**.css`)
+  .pipe(gulp.dest(paths.styles.destination)));
 
 gulp.task('templates', () => gulp.src(`${paths.templates.source}/**.html`)
   .pipe(gulp.dest(paths.templates.destination)));
 
-gulp.task('build', gulp.series('clean', gulp.parallel('scripts', 'templates')));
+gulp.task('build', gulp.series('clean', gulp.parallel('images', 'scripts', 'styles', 'templates')));
 gulp.task('default', gulp.series('build'));
 
 gulp.task('watch', gulp.series('default', () => {
@@ -59,6 +73,8 @@ gulp.task('watch', gulp.series('default', () => {
     server: './dist',
   });
 
+  gulp.watch(`${paths.images.source}/**.js`, gulp.series('images', reload));
   gulp.watch(`${paths.scripts.source}/**.js`, gulp.series('scripts', reload));
+  gulp.watch(`${paths.styles.source}/**.css`, gulp.series('styles', reload));
   gulp.watch(`${paths.templates.source}/**.html`, gulp.series('templates', reload));
 }));
