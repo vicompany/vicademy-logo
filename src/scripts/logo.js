3b16e7e60018aeac218fb2fff8b87246945ca814
Tim Severien <timseverien@gmail.com>
2017-04-11 15:25:27 +0200

    Remove comment

--- src/scripts/logo.js
+++ src/scripts/logo.js
@@ -13,33 +13,3 @@ export default class Logo {
     this.element.setAttribute('d', path);
   }
 }
-
-// const presetList = document.querySelector('.js-preset-list');
-// const presetButtons = Array.from(presetList.querySelectorAll('.js-preset-button'));
-// const logoPath = document.querySelector('.js-logo-path');
-// const today = formatDate(new Date());
-
-// // Set logo background
-// const logoPathBackground = document.querySelector('.js-logo-background');
-// logoPathBackground.setAttribute('d', getLogoPath(hexagon, [0, 0, 0, 0, 0, 0], false));
-
-// // Update today preset
-// document.querySelector('.js-preset-today').dataset.date = today;
-// document.querySelector('.js-preset-today-display').innerHTML = today;
-
-// // Add interactivity for preset list
-// presetList.addEventListener('click', (e) => {
-//   if (e.target.tagName !== 'BUTTON') return;
-
-//   const button = e.target;
-//   const date = button.dataset.date;
-
-//   presetButtons.forEach(b => b.classList.remove('is-active'));
-//   button.classList.add('is-active');
-
-//   updatePath(logoPath, date);
-// });
-
-// // Select first item from list
-// updatePath(logoPath, presetList.querySelector('button[data-date]').dataset.date);
-
